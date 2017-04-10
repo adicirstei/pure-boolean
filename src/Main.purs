b1de6ff986add358a833d6d1faa4d69fba5f8f00
Adrian Cirstei <adicirstei@gmail.com>
2017-04-10 17:05:25 +0300

    managed to read the args

--- src/Main.purs
+++ src/Main.purs
@@ -7,19 +7,29 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 import Node.HTTP.Client as Client
 import Node.HTTP (HTTP)
 
+import Data.Array (drop)
+
+import Node.Process as Proc
+
 redditUri = "https://www.reddit.com/r/purescript.json"
 
 --redditReq = Client.requestFromURI redditUri \response -> void
 
+--
+-- callback :: forall e. Client.Response -> Eff ( http :: HTTP | e) Unit
+-- callback res =
 
-callback :: forall e. Client.Response -> Eff ( http :: HTTP | e) Unit
-callback res = pure
+
+
+
 
 req :: forall t2. (Client.Response -> Eff ( http :: HTTP | t2) Unit)
   -> Eff ( http :: HTTP | t2 ) Client.Request
 req = Client.requestFromURI redditUri
 
 
-main :: forall e. Eff (console :: CONSOLE, http :: HTTP | e) Unit
+main :: forall e. Eff (console :: CONSOLE, http :: HTTP, process :: Proc.PROCESS | e) Unit
 main = do
+  args <- Proc.argv
   log "Hello sailor!"
+  log $ show $ drop 2 args 
