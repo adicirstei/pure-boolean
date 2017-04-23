9ec5df4db98a962813c99ed8165c5df7814cac50
Adrian Cirstei <adicirstei@gmail.com>
2017-04-23 21:07:43 +0300

    working version

--- src/Main.purs
+++ src/Main.purs
@@ -2,22 +2,43 @@ module Main where
 
 import Prelude
 import HTTP.Request
+import Spotify
 import Node.Process as Proc
+import Control.Bind ((=<<))
 import Control.Monad.Aff (Aff, Canceler, launchAff, makeAff, runAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
---import Data.Array (drop, intersect)
-import Node.Process (PROCESS)
+import Data.Array (drop, intersect)
+import Data.Either (Either(..))
+import Data.Eq (class Eq)
+import Data.List (List(..), (:), fromFoldable)
+import Data.Traversable (traverse)
+import Node.Process (PROCESS, argv)
 
 
-redditAff :: forall e. Aff ( ajax :: AJAX | e) String
-redditAff = makeAff $ httpGet "https://www.reddit.com/r/purescript.json"
+names =
+  Proc.argv 
+  <#> drop 2
 
---
--- callback :: forall e. Client.Response -> Eff ( http :: HTTP | e) Unit
--- callback res =
+
+related :: forall e. String -> Aff ( ajax :: AJAX, console :: CONSOLE | e) (Array String)
+related name = do
+  eitherArtist <- findArtist name
+  case eitherArtist of
+    Left _ -> pure []
+    Right a -> do 
+     liftEff $ log (a.name)
+     artistsEither <- relatedArtists (a.id)
+     case artistsEither of
+       Left _ -> pure []
+       Right xs -> pure (map _.name xs)
+
+intersection :: forall a. Eq a => List (Array a) -> Array a
+intersection Nil = []
+intersection (x : Nil) = x
+intersection (x : y : xss) = intersection ((intersect x y) : xss)
 
 
 
@@ -28,26 +49,7 @@ main :: forall e.
       ( Canceler ( ajax :: AJAX, console :: CONSOLE, process :: PROCESS | e ) )
 main = 
   launchAff $ do
-    
-    redStr <- redditAff
-    liftEff $ do 
-      args <- Proc.argv
-      log redStr
-      log $ show args
-
-
-
-
--- main =
---   bind 
---     Proc.argv
---     (\args -> 
---       discard 
---         (log "Here comes the aguments") 
---         (\_ -> log $ show args)
-    
---     )
-    
-
-  
-
+    names <- liftEff names
+    rels <- traverse related names
+    --suggestions <- foldMap int
+    liftEff $ log $ show (intersection $ fromFoldable rels)
